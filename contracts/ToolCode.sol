341aa2f07e44a3187fdf25b5cdb40f004bf329bf
minosletitgo <380122248@qq.com>
2024-11-10 18:22:19 +0800

    1

--- contracts/ToolCode.sol
+++ contracts/ToolCode.sol
@@ -1,37 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
-contract ToolCode{
-
-    function encodeWithSelector() public pure returns(bytes memory) {
+contract ToolCode {
+    function encodeWithSelector() public pure returns (bytes memory) {
         bytes4 data = bytes4(keccak256("setValue4(uint256)"));
         bytes memory encodedData = abi.encodeWithSelector(data, 123);
-        return encodedData;
-    } 
-
-    /////////////////////////////////////////////////////////////
-
-    function encode_burn() public pure returns(bytes memory) {
-        bytes4 data = bytes4(keccak256("burn(uint256)"));
-        bytes memory encodedData = abi.encodeWithSelector(data, 7);
-        return encodedData;
-    } 
-
-    function encode_collate_propagate_storage() public pure returns(bytes memory) {
-        bytes4 data = bytes4(keccak256("collate_propagate_storage(bytes16)"));
-        bytes memory encodedData = abi.encodeWithSelector(data, 7);
         return encodedData;
     }
 
     /////////////////////////////////////////////////////////////
 
-    function encode_upgradeTo(address target) public pure returns(bytes memory) {
+    function encode_burn() public pure returns (bytes memory) {
+        bytes4 data = bytes4(keccak256("burn(uint256)"));
+        bytes memory encodedData = abi.encodeWithSelector(data, 123);
+        return encodedData;
+    }
+
+    function encode_collate_propagate_storage()
+        public
+        pure
+        returns (bytes memory)
+    {
+        bytes4 data = bytes4(keccak256("collate_propagate_storage(bytes16)"));
+        bytes memory encodedData = abi.encodeWithSelector(data, 123);
+        return encodedData;
+    }
+
+    /////////////////////////////////////////////////////////////
+
+    function encode_upgradeTo(address target)
+        public
+        pure
+        returns (bytes memory)
+    {
         bytes4 data = bytes4(keccak256("upgradeTo(address)"));
         bytes memory encodedData = abi.encodeWithSelector(data, target);
         return encodedData;
-    }     
+    }
 
     /////////////////////////////////////////////////////////////
 
@@ -39,13 +44,19 @@ contract ToolCode{
 
     function burn(uint256 value) public {
         emit Burned(msg.sender, value);
-        console.log("burn %d", value);
     }
 
     /////////////////////////////////////////////////////////////
 
     error ReentrancyGuardReentrantCall();
+
     function foo() external pure {
         revert ReentrancyGuardReentrantCall();
     }
+
+    /////////////////////////////////////////////////////////////
+
+    function doSomething() public {}
+
+    function doSomething(uint256 value) public {}
 }
--- test/runToolCode.js
+++ test/runToolCode.js
@@ -1,23 +1,25 @@
 const hre = require("hardhat");
 const logger = require("../srcs/logger");
 require("dotenv").config();
-const contractABI = require("../abi/ToolCode.json");
 
 async function main() {
-  // 定义目标合约的函数签名
-  const functionSignature = "burn(uint256)";
+  {
+    // 示例：无参函数
+    const contract = new ethers.utils.Interface(["function doSomething()"]);
+    const selector = contract.getSighash("doSomething");
+    const encodedData = contract.encodeFunctionData("doSomething", []);
+    console.log("Encoded Data With Null:", encodedData);
+    // 返回值是：0x82692679
+  }
 
-  // 创建合约接口实例
-  const contractAbi = ["function burn(uint256)"];
-  const contract = new ethers.utils.Interface(contractAbi);
-
-  // 计算函数选择器
-  const selector = contract.getSighash("burn");
-
-  // 编码数据
-  const encodedData = contract.encodeFunctionData("burn", [123]);
-
-  console.log("Encoded Data:", encodedData);
+  {
+    // 示例：有参函数，为123
+    const contract = new ethers.utils.Interface(["function doSomething(uint256)"]);
+    const selector = contract.getSighash("doSomething");
+    const encodedData = contract.encodeFunctionData("doSomething", [123]);
+    console.log("Encoded Data With 123:", encodedData);
+    // 返回值是：0xa6b206bf000000000000000000000000000000000000000000000000000000000000007b
+  }  
 }
 
 main()
@@ -28,5 +30,5 @@ main()
   });
 
 /*
-    npx hardhat run .\test\runTestCode.js --network localGanache
+    npx hardhat run .\test\runToolCode.js --network localGanache
 */
